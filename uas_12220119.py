f43ee5a50860d9b454eecb86e16c407523ff56b7
zackymulatif <96175270+zackymulatif@users.noreply.github.com>
2021-12-17 12:55:09 +0700

    Add files via upload

--- uas_12220119.py
+++ uas_12220119.py
@@ -280,7 +280,7 @@ FITUR = OrderedDict(
 
 
 def run():
-    demo_name = st.sidebar.selectbox("Silahkan pilih menu", list(FITUR.keys()), 0)
+    demo_name = st.sidebar.selectbox("Menu", list(FITUR.keys()), 0)
 
     demo = FITUR[demo_name][0]
     if demo_name == "Home":
